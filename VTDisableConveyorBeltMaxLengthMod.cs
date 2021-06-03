2bc8489edd3eec19b84f430e561e78c8484c439f
ianmcderp <matthias@riedl.rocks>
2021-06-03 12:24:48 +0200

    Initial version of the mod.

--- VTDisableConveyorBeltMaxLengthMod.cs
+++ VTDisableConveyorBeltMaxLengthMod.cs
@@ -1,8 +1,42 @@
-﻿using VoxelTycoon.Modding;
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using VoxelTycoon;
+using VoxelTycoon.Modding;
 
 namespace VTDisableConveyorBeltMaxLengthMod
 {
     public class VTDisableConveyorBeltMaxLengthMod : Mod
     {
+        Logger _logger = new Logger<VTDisableConveyorBeltMaxLengthMod>();
+
+        protected override void OnGameStarted()
+        {
+            try
+            {
+                var harmony = new Harmony("com.github.ianmcderp");
+                Harmony.DEBUG = true;
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                harmony.PatchAll();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Loading VTDisableConveyorBeltMaxLengthMod failed");
+                _logger.LogException(e);
+            }
+        }
+    }
+
+    [HarmonyPatch(
+        typeof(VoxelTycoon.Tools.TrackBuilder.ConveyorBuilderTool),
+        "CheckLength",
+        new Type[] {}
+        )]
+    class PatchedConveyorBuilderTool
+    {
+        static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
     }
 }
